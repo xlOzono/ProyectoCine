7481faff2f46a5b95f65b813282ad21bd3ac152f
Melisa Huanca <melisa.huanca.laura@alumnos.uta.cl>
2024-11-22 09:58:23 -0300

    Funciona el editar

--- src/app/components/video-player/video-player.component.ts
+++ src/app/components/video-player/video-player.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FunAdminService } from 'src/app/fun-admin.service';
 
 @Component({
   selector: 'app-video-player',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./video-player.component.css']
 })
 export class VideoPlayerComponent {
+  constructor(public funcion: FunAdminService) {}
 
+  openVideoDialog(): void {
+    const url = prompt('Ingresa el enlace del video de YouTube:');
+    if (url && this.isValidYouTubeUrl(url)) {
+      this.funcion.addTrailer(url);
+      alert('Enlace agregado exitosamente');
+    } else if (url) {
+      alert('Por favor, ingresa un enlace válido de YouTube.');
+    }
+  }
+
+  private isValidYouTubeUrl(url: string): boolean {
+    const regex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+    return regex.test(url);
+  }
 }
--- src/app/fun-admin.service.ts
+++ src/app/fun-admin.service.ts
@@ -7,10 +7,15 @@ export class FunAdminService {
   constructor() { }
 
   funciones: string[] = [];
+  trailers:string[] = [];
   
   add(sala: string, horario: string){ 
-    /*lo que esta adentro es simplemento como nos referiremos al 
+    /*lo que esta adentro es simplemente como nos referiremos al 
     parametro dentro de la funcion*/
     this.funciones.push(horario + " | " + sala);
-  } 
+  }
+
+  addTrailer(link: string) {
+    this.trailers.push(link);
+  }
 }
